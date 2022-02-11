e124ac26fc739fb1d748698f8f6cf3335c17fac0
汪航洋 <wanghangyang@szewec.com>
2022-02-11 18:33:29 +0800

    v1.0.0-beta.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
-import typescript from 'rollup-plugin-typescript';
+// import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import { uglify } from 'rollup-plugin-uglify';
 
@@ -6,7 +7,7 @@ import { uglify } from 'rollup-plugin-uglify';
 // import es3 from 'rollup-plugin-es3'
 import pkg from './package.json';
 
-const name = "qrcodeMatrix"
+const name = 'qrcodeMatrix';
 const banner = `/*!
  * ${pkg.name}  v${pkg.version}
  * Homepage ${pkg.homepage}
@@ -14,62 +15,56 @@ const banner = `/*!
  */
 `;
 let minOpts = {
-	output: {
-		preamble: banner
-	}
+  output: {
+    preamble: banner,
+  },
 };
 
 let external = Object.keys(pkg.dependencies);
 let plugins_typeES2015 = typescript({
-	target: 'ES2015',
-	module: 'ES2015',
-	removeComments: true
+  target: 'ES2015',
+  module: 'ES2015',
+  removeComments: true,
 });
 
 let out_config = [
-	{
-		input: './src/index.ts',
-		plugins: [plugins_typeES2015],
-		output: [
-			{
-				file: pkg.module,
-				format: 'es',
-				banner: banner
-			},
-			{
-				file: pkg.main,
-				format: 'cjs',
-				exports: 'named'
-			}
-		],
-		external
-	},
-	{
-		input: pkg.module,
-		plugins: [nodeResolve()],
-		output: [
-			{
-				file: pkg.browser,
-				format: 'umd',
-				name: name,
-				exports: 'named'
-			}
-		]
-	},
-	// {
-	// 	input: pkg.module,
-	// 	plugins: [
-	// 		uglify(minOpts),
-	// 	],
-	// 	output: [
-	// 		{
-	// 			file: pkg.browser.replace(/\.js$/, '.min.js'),
-	// 			name: name,
-	// 			format: 'umd',
-	// 			exports: 'named'
-	// 		}
-	// 	]
-	// }
+  {
+    input: './src/index.ts',
+    plugins: [plugins_typeES2015],
+    output: [
+      {
+        file: pkg.module,
+        format: 'es',
+        banner: banner,
+      },
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+		    banner: banner,
+      },
+    ],
+    external,
+  },
+  {
+    input: pkg.module,
+    plugins: [nodeResolve()],
+    output: [
+      {
+        file: pkg.browser,
+        format: 'umd',
+        name: name,
+        exports: 'named',
+      },
+      {
+        file: pkg.browser.replace(/\.js$/, '.min.js'),
+        name: name,
+        format: 'umd',
+        exports: 'named',
+        plugins: [uglify(minOpts)],
+      },
+    ],
+  },
 ];
 
 export default [...out_config];
---
+++ test/demo.js
@@ -0,0 +1,36 @@
+var QRCode = require('../').default;
+var qrcode = QRCode({
+	errorCorrectionLevel: 'M', //纠错等级 默认M   'L','M','Q','H' -> [1,0,3,2]
+	typeNumber: 2, //QR码版本 默认0 (自动)  1 to 40
+	minTypeNumber: 0, //最小typeNumber版本,当typeNumber自动时才有效
+	maskPattern: 'auto', //掩模 'random','auto'  [0-7]
+	dataMode: 'Byte' //默认数据类型, 参看 QRCode.QRModes 对像
+});
+qrcode.setData('1234');
+var matrix = qrcode.make(); // 一个布尔值的二维数组
+var splitMake = qrcode.splitMake()
+console.log(splitMake);
+var attr = [
+	`all`,
+	`data`,
+	`allDiscover`,
+	`positionProbe`,
+	`positionAdjust`,
+	`timing`,
+]
+//在控制台打印出二维码试试 (我的控制台是黑色背景,所以我加上了一圈边框)
+var $0 = '8 ',
+    $1 = '  ';
+function showV(matrix){
+	console.log($0.repeat(matrix.width + 2));
+	for(var r =0; r<matrix.height; r++){
+		let row = matrix.getRow(r);
+		let str = row.map(item => (item ? $1 : $0)).join('');
+		console.log($0 + str + $0);
+	}
+	console.log($0.repeat(matrix.width + 2));
+}
+attr.map(att=>{
+	console.log(att);
+	splitMake[att] && showV(splitMake[att]);
+})
